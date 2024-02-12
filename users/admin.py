cf6eadb06d233f7b9a63641e3f9c51ee9bce3700
Tafadzwa Choga <tafIeearl@gmail.com>
2024-02-12 10:51:42 +0000

    added models under users

--- users/admin.py
+++ users/admin.py
@@ -3,12 +3,30 @@ from main.models import Course
 from .models import *
 
 # Register your models here.
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ('course_name', 'course_code', 'course_fee', 'course_status', 'course_date_created')
-    list_filter = ('course_status', 'course_date_created')
-    search_fields = ('course_name', 'course_code')
 
-admin.site.register(Course, CourseAdmin)
+
+class IMUserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'user_type', 'is_active', 'date_created')
+    list_filter = ('user_type', 'is_active')
+    search_fields = ('first_name', 'last_name')
+
+class CohortAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'is_active', 'author', 'start_date', 'end_date')
+    list_filter = ('year', 'is_active')
+    search_fields = ('name', 'description')
+    raw_id_fields = ('author',)
+
+class CohortMemberAdmin(admin.ModelAdmin):
+    list_display = ('member', 'cohort', 'is_active', 'author', 'date_created')
+    list_filter = ('is_active',)
+    search_fields = ('member__first_name', 'member__last_name', 'cohort__name')
+    raw_id_fields = ('member', 'cohort', 'author')
+
+# Register the models with the admin and specify the custom ModelAdmin classes
+admin.site.register(IMUser, IMUserAdmin)
+admin.site.register(Cohort, CohortAdmin)
+admin.site.register(CohortMember, CohortMemberAdmin)
+
 
 
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class IMUser(models.Model):
+    USER_TYPE_CHOICES = [
+        ('EIT', 'entrepreneur in training'),
+        ('TEACHING_FELLOW', 'Teaching Fellow'),
+        ('ADMIN_STAFF', 'Administrative Staff'),
+        ('ADMIN', 'Administrator'),
+    ]
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Cohort(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    year = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(IMUser, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+class CohortMember(models.Model):
+    cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE)
+    member = models.ForeignKey(IMUser, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(IMUser, on_delete=models.SET_NULL, null=True, related_name='+')
+
+    
+
+    def __str__(self):
+        return f'{self.member.first_name} {self.member.last_name} - {self.cohort.name}'
